1696c1bd4d22bfb9f4033453e666a3c00fadf606
РИШАТ Ахтямов <rishat-rus@mail.ru>
2020-11-16 10:44:10 +0400

    Использование тем в wpf создание юнит теста

---
+++ CompanyCoreLib/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyCoreLib
+{
+    /// <summary>
+    /// sfdkjdfgjkjk sdkjlfdkjlfljk fk
+    /// </summary>
+    public class Analytics
+    {
+        /// <summary>
+/// Это метод выдает популярные месяцы
+/// </summary>
+/// <param name="dates"></param>
+/// <returns></returns>
+        public List<DateTime> PopularMonths(List<DateTime> dates)
+        {
+
+            return new List<DateTime>();
+        }
+        
+    }
+}
